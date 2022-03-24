a1997bf7720279f80a1db4a4a51a09a025f14290
jyb1798 <bestdung8509@gmail.com>
2022-03-24 18:54:04 +0900

    Add: 데이터 바인딩 test

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,35 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+   <div>
+      <h4>{{products[0]}}</h4>
+      <p>{{price2 }} 만원</p>
+   </div>
+    <div>
+      <h4>{{products[1]}}</h4>
+      <p>{{price1}} 만원</p>
+   </div>
+       <div>
+      <h4>{{products[2]}}</h4>
+      <p>{{price1}} 만원</p>
+   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
+  name : 'App',
+  data(){
+    return {
+      price1 : 80,
+     price2 : 50,
+     products: ['역삼동원룸', '천호동원름', '마포구원룸']
+    }
+  },
   components: {
-    HelloWorld
+
   }
 }
+
 </script>
 
 <style>
